f16d04989d7b6f0fe1c32feeccc3bb8491b44cdb
Jhappy77 <joel.happ@shaw.ca>
2020-11-29 01:34:12 -0700

    prepare to make views

--- swankyDB/views.py
+++ swankyDB/views.py
@@ -12,8 +12,15 @@ from .serializers import *
 from datetime import datetime, timedelta
 
 
+## Don't do anything with the User Model just yet. Django has some predefined user
+## stuff that we should probably use instead, I will look into it tomorrow morning
+
 # Saves a licenses' information
+# (This is a template view so we don't have to do much work. More complex views will require more work)
 class saveLicense(generics.CreateAPIView):
     queryset = License.objects.all()
-    serializer_class = LicenseCreateSerializer
+    serializer_class = LicenseSerializer
+
+
+# If you want to make views, you can see the views I made at https://github.com/Jhappy77/swagadellicAPI/tree/main/swagDB in views.py for inspiration
 
